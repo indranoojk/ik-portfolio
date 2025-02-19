2935a8aa08f4df3921281ec6110ce8bd2bb482b0
indranoojk <indranoojk@gmail.com>
2025-02-19 16:06:25 +0530

    react version change

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,23 +1,8 @@
-import React from 'react';
-import { Html, useProgress } from '@react-three/drei';
+import React from 'react'
 
 const Loader = () => {
-
-  const { progress } = useProgress();
-
   return (
-    <Html>
-      <span className="canvas-load"></span>
-      <p 
-        style={{
-          fontSize: 14,
-          color: '#f1f1f1',
-          fontWeight: 800,
-          marginTop: 14
-        }}>
-        {progress.toFixed(2)}%
-      </p>
-    </Html>
+    <div>Loader</div>
   )
 }
 
