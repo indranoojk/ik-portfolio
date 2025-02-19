62e3b18d596d1e9a47631175a2b464ea508a14d1
indranoojk <indranoojk@gmail.com>
2025-02-19 17:18:57 +0530

    responsiveness updated

--- src/App.jsx
+++ src/App.jsx
@@ -47,7 +47,7 @@ const App = () => {
           ></div>
 
           <div className="relative h-full w-full">
-            <Navbar />
+            <Navbar toggle={false} />
             <Hero />
             <About />
             <Experience />
